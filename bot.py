469de40007faf50d2dd72315b4900c02c8ddd257
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-09-16 00:26:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,43 +21,6 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 moment_worker = []
 
 
-#start
-@client.on(events.NewMessage(pattern="^/start$"))
-async def start(event):
-  await event.reply("^_^ Hey, Welcome To TAG Help Bot's Menu\nI can tag 15,000 Members in Group and 300 Members In Channel.\nNeed Help /help ",
-                    buttons=(
-                      [
-                         Button.url('📣 UPDATES', 'https://t.me/DeeCodeBots'), 
-                         Button.url('⭐SUPPORT', 'https://t.me/DeCodeSupport'), 
-                      ], 
-                      [
-                        Button.url('➕ ADD ME TO YOUR GROUP', 'https://t.me/MEMBER_TAGERBOT?startgroup=true'),   
-                      ]
-                   ), 
-                    link_preview=False
-                   )
-
-#help
-@client.on(events.NewMessage(pattern="^/help$"))
-async def help(event):
-  helptext = "**Tag Help Bot's Help Menu**\n\nCommand: /all \n You can use this command with text you want to tell others. \n`Example: /all Good morning!` \nYou can use this command as an answer. any message Bot will tag users to replied message"
-  await event.reply(helptext,
-                    buttons=(
-                      [
-                         Button.url('📣 UPDATES', 'https://t.me/DeeCodeBots'), 
-                         Button.url('⭐SUPPORT', 'https://t.me/DeCodeSupport'), 
-                      ], 
-                      [
-                        Button.url('➕ ADD ME TO YOUR GROUP', 'https://t.me/MEMBER_TAGERBOT?startgroup=true'),   
-                      ]
-                   ), 
-                    link_preview=False
-                   )
-
-#Wah bhaiya full ignorebazzi
-
-#bsdk credit de dena verna maa chod dege
-
 #tag
 @client.on(events.NewMessage(pattern="^/tagall|/call|/tall|/all|#all|@all?(.*)"))
 async def mentionall(event):
@@ -117,49 +80,6 @@ async def mentionall(event):
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
-
-
-#telegraph 
-@client.on(events.NewMessage(pattern="^/t$"))
-async def telegraph(client, message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply("Reply to a supported media file")
-        return
-    if not (
-        (replied.photo and replied.photo.file_size <= 5242880)
-        or (replied.animation and replied.animation.file_size <= 5242880)
-        or (
-            replied.video
-            and replied.video.file_name.endswith(".mp4")
-            and replied.video.file_size <= 5242880
-        )
-        or (
-            replied.document
-            and replied.document.file_name.endswith(
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
-            )
-            and replied.document.file_size <= 5242880
-        )
-    ):
-        await message.reply("Not supported!")
-        return
-    download_location = await client.download_media(
-        message=message.reply_to_message,
-        file_name="root/downloads/",
-    )
-    try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply(message, text=document)
-    else:
-        await message.reply(
-            f"**Hey You...!\nLoook At This\n\n👉 https://telegra.ph{response[0]}**",
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
-
 
 
 print("Started Successfully Join Support")
